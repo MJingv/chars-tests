4f48dc4b674759d6a3928859a93b0185c62a77b5
majing_i <majing_i@didichuxing.com>
2018-01-22 14:49:38 +0800

    add test

--- echars-tests/src/App.js
+++ echars-tests/src/App.js
@@ -115,7 +115,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome test </h1>
+                    <h1 className="App-title">Welcome</h1>
                 </header>
                 <section>
                     {/*<div className='main' ref='bar'></div>*/}
